d0f04be018820cbc0b7057bcf9a6affcc5bad810
cooper <tokyolabs@protonmail.com>
2023-10-02 10:20:40 -0400

    fixed wait for db error

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,16 +15,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write(self.style.WARNING("Waiting for database..."))
+        start_time = time.time()
+        timeout = 60  # so it doesn't try indefinitely if there's a persistent issue with the database connection
+        wait_time = 1
         db_up = False
-        while db_up is False:
+        while db_up is False and time.time() - start_time < timeout:
             try:
-                self.check(databases=["default"])
+                self.check()
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write(
                     self.style.ERROR("db unavailable, waiting 1 second...")
                 )
 
-                time.sleep(1)
+                time.sleep(wait_time)
+                wait_time = min(
+                    wait_time * 2, 30
+                )  # Double the wait time, but cap at 30 seconds
 
         self.stdout.write(self.style.SUCCESS("db available!"))
