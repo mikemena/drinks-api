888fbe4a0384e1c07a8b5b149d529f77344a6eaf
cooper <tokyolabs@protonmail.com>
2023-10-03 09:35:51 -0400

    added superuser feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,3 +36,9 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user("", "test123")
+
+    def test_superuser(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
