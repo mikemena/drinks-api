ed386e699d1c5d79218b9c4403c95a162d78ce53
cooper <tokyolabs@protonmail.com>
2023-10-04 18:39:10 -0400

    fixed spacing causing lint errors

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,4 @@
-"""Serializers for the user API View."""
+"""Serializers for the user API View. """
 
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext as _
