48912268e8ef475b9a33546dc7675ce948a782dd
cooper <tokyolabs@protonmail.com>
2023-10-07 12:32:38 -0400

    Implemented tag isting API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,10 @@
 """Views for the recipe API"""
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -30,3 +30,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage recipe tags in the databse."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filet queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
